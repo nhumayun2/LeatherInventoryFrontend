c8e94a202fb3ae4359483a425a4b806321e7cd67
nhumayun2 <nhumayun291@gmail.com>
2026-04-27 23:52:47 +0600

    moved to live

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
 
   // 💻 For Local Development
-  apiUrl: 'https://localhost:7201/api',
+  //apiUrl: 'https://localhost:7201/api',
 
   // ☁️ For Live Production
   // apiUrl: 'https://karigar-api.onrender.com/api',
